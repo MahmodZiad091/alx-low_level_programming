7d8e2a7f06c77cccb4ee38daa4b7827ad9fd4a41
Mahmodziad091 <mahmodalkassem20000@gmail.com>
2023-07-18 14:34:53 -0700

    .

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+    int a;
+    print_alphabet(a);
+    return (0);
+}
+
+void print_alphabet(void);
+{
+	char a = alphabet
+	puts("%s", a)
+}
